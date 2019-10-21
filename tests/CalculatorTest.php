ed99e741314ecd22b454a042b7e16c80ded46c75
Ronny Hartenstein <r.hartenstein@axilaris.de>
2019-10-21 10:30:12 +0200

    10 durch 3

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -8,8 +8,13 @@ use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase
 {
-    public function test_meep(): void
+    public function test_ergebnis_10_durch_2_ist_5(): void
     {
         $this->assertSame(5, 10 / 2);
+    }
+
+    public function test_10_durch_3_ist_3_und_nochwas(): void
+    {
+        $this->assertSame(3.3333333333333335, 10 / 3);
     }
 }
